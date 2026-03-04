712071c4353003556deaa7f0aea1bad774ef04aa
zrob56 <131211905+zrob56@users.noreply.github.com>
2026-03-04 13:43:06 -0700

    Update prewarm.js

--- api/stremio/prewarm.js
+++ api/stremio/prewarm.js
@@ -1,7 +1,7 @@
 import { Redis } from '@upstash/redis';
 
 const TRAKT_BASE = 'https://api.trakt.tv';
-const GEMINI_BASE = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent';
+const GEMINI_BASE = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent';
 
 const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
