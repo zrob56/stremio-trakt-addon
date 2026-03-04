b3029d41e398662c8d8d1755a87ca5d2ed850083
zrob56 <131211905+zrob56@users.noreply.github.com>
2026-03-04 02:09:56 -0700

    Update handler.js

--- api/stremio/handler.js
+++ api/stremio/handler.js
@@ -202,7 +202,7 @@ function handleManifest(config, res) {
     name: 'Trakt Recommendations',
     description: 'AI-powered movie & show recommendations by genre, powered by your Trakt history.',
     logo: 'https://www.cnet.com/a/img/resize/0e9874cc9d6b18489f832793796d285141496106/hub/2021/10/16/11804578-0dbc-42af-bcd1-3bc7b1394962/the-batman-2022-teaser-poster-batman-01-promo.jpg?auto=webp&fit=bounds&height=900&precrop=1881,1411,x423,y0&width=1200',
-    resources: ['catalog', 'meta'],
+    resources: ['catalog'],
     types: ['movie', 'series'],
     catalogs,
     behaviorHints: { configurable: true },
